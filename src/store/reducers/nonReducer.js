5e4e796210ef115c01e4c17b26f992d067beba68
bez <abezlife@gmail.com>
2020-05-16 19:47:46 -0700

    setState

--- src/store/reducers/nonReducer.js
+++ src/store/reducers/nonReducer.js
@@ -1,17 +1,24 @@
-class StatefulComponent extends React.Component {
+class MyComponent extends React.Component {
     constructor(props) {
-      super(props);
-      // initialize state here
-  this.state = {
-      name='::'
-  }
+        super(props);
+        this.state = {
+            name: 'Initial State'
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
+    handleClick() {
+         
+        this.setState({
+            name: '  React Rocks!'
+        });;
+         
+    };
     render() {
-      return (
-        <div>
-          <h1>{this.state.name}</h1>
-        </div>
-      );
+        return (
+            <div>
+                <button onClick={this.handleClick}>Click Me</button>
+                <h1>{this.state.name}</h1>
+            </div>
+        );
     }
-  };
-  
+};
